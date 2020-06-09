ed0d4625f7eaf9593f893cf390370e403c6bfae2
Abhinav Valecha <valecha.abhinav@gmail.com>
2020-06-09 21:05:15 +0530

    Fix test for single number in input

--- src/main/java/abhinav/tdd/kata/StringCalculator.java
+++ src/main/java/abhinav/tdd/kata/StringCalculator.java
@@ -3,6 +3,9 @@ package abhinav.tdd.kata;
 public class StringCalculator
 {
     public int add(final String numbers) {
+        if (!numbers.isEmpty()) {
+            return Integer.parseInt(numbers);
+        }
         return 0;
     }
 }
