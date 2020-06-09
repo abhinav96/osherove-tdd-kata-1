99d77885bbde9fa5a759bec1f8f42e351c12af96
Abhinav Valecha <valecha.abhinav@gmail.com>
2020-06-09 21:25:43 +0530

    Fix test for adding two numbers

--- src/main/java/abhinav/tdd/kata/StringCalculator.java
+++ src/main/java/abhinav/tdd/kata/StringCalculator.java
@@ -4,7 +4,12 @@ public class StringCalculator
 {
     public int add(final String numbers) {
         if (!numbers.isEmpty()) {
-            return Integer.parseInt(numbers);
+            if (!numbers.contains(",")) {
+                return Integer.parseInt(numbers);
+            }
+            final int firstNum = Integer.parseInt(numbers.substring(0, numbers.indexOf(",")));
+            final int secondNum = Integer.parseInt(numbers.substring(numbers.indexOf(",") + 1));
+            return firstNum + secondNum;
         }
         return 0;
     }
