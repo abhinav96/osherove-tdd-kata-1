69481a26986ea28dad42df98973ac1c588f29354
Abhinav Valecha <valecha.abhinav@gmail.com>
2020-06-09 21:41:19 +0530

    Add tests for numbers with extra spaces

--- src/test/java/abhinav/tdd/kata/StringCalculatorTest.java
+++ src/test/java/abhinav/tdd/kata/StringCalculatorTest.java
@@ -28,4 +28,26 @@ public class StringCalculatorTest
         final int sum = calculator.add("1,2");
         assertEquals(3, sum);
     }
+
+    @Test
+    public void onlySpacesShouldReturnZero() {
+        final StringCalculator calculator = new StringCalculator();
+        final int answer = calculator.add("   ");
+        assertEquals(0, answer);
+    }
+
+    @Test
+    public void singleNumberWithExtraSpacesShouldReturnTheNumber() {
+        final StringCalculator calculator = new StringCalculator();
+        final int answer = calculator.add("  1 ");
+        assertEquals(1, answer);
+    }
+
+    @Test
+    public void twoNumbersWithSpacesShouldReturnTheirSum() {
+        final StringCalculator calculator = new StringCalculator();
+        final int answer = calculator.add(" 1 , 2 ");
+        assertEquals(3, answer);
+    }
+
 }
