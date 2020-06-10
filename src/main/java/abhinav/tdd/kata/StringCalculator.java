643c2f46a8d7fff16c22d32e9baeed40b1f917de
Abhinav Valecha <valecha.abhinav@gmail.com>
2020-06-10 10:50:04 +0530

    Refactor StringCalculator

--- src/main/java/abhinav/tdd/kata/StringCalculator.java
+++ src/main/java/abhinav/tdd/kata/StringCalculator.java
@@ -8,22 +8,13 @@ public class StringCalculator
 
     public int add(final String numbers) {
         if (!numbers.trim().isEmpty()) {
-            if (isSingleNumber(numbers)) {
-                return Integer.parseInt(numbers.trim());
-            }
             final String[] separatedNums = numbers.split(SEPARATOR);
-            return Arrays.stream(separatedNums).map(n -> n.trim()).mapToInt(Integer::parseInt).sum();
+            return Arrays.stream(separatedNums)
+                    .map(n -> n.trim())
+                    .mapToInt(Integer::parseInt)
+                    .sum();
         }
         return 0;
     }
 
-    private int addTwoNumbers(final String numbers) {
-        final int firstNum = Integer.parseInt(numbers.substring(0, numbers.indexOf(SEPARATOR)).trim());
-        final int secondNum = Integer.parseInt(numbers.substring(numbers.indexOf(SEPARATOR) + 1).trim());
-        return firstNum + secondNum;
-    }
-
-    private boolean isSingleNumber(final String numbers) {
-        return !numbers.contains(SEPARATOR);
-    }
 }
