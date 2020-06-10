c7721797e510dfc6041938a95a799b5b824edbc8
Abhinav Valecha <valecha.abhinav@gmail.com>
2020-06-10 12:58:00 +0530

    Add support for custom separator

--- src/main/java/abhinav/tdd/kata/StringCalculator.java
+++ src/main/java/abhinav/tdd/kata/StringCalculator.java
@@ -1,14 +1,21 @@
 package abhinav.tdd.kata;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class StringCalculator
 {
-    private static final String SEPARATOR = "[,\n]";
+    private static final String DEFAULT_SEPARATOR = "[,\n]";
 
     public int add(final String numbers) {
         if (!numbers.trim().isEmpty()) {
-            final String[] separatedNums = numbers.split(SEPARATOR);
+            String input = numbers;
+            String separator = DEFAULT_SEPARATOR;
+            if (numbers.startsWith("//")) {
+                separator = Pattern.quote(numbers.substring(2, 3));
+                input = numbers.substring(4);
+            }
+            final String[] separatedNums = input.split(separator);
             return Arrays.stream(separatedNums)
                     .map(n -> n.trim())
                     .mapToInt(Integer::parseInt)
